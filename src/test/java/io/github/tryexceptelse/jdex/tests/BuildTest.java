ffd8ef296994421a77a0c706810ce3a19c8fe688
TryExcept <schwarzjohnv@gmail.com>
2016-05-21 03:05:02 -0700

    fixed build test

---
+++ src/test/java/io/github/tryexceptelse/jdex/tests/BuildTest.java
@@ -0,0 +1,26 @@
+package io.github.tryexceptelse.jdex.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * Simply test that project is able to be built correctly.
+ * 
+ * Reminder for tests:
+ * Classes and methods must begin or end in 'Test' plural 'Tests' will 
+ * not work
+ */
+public class BuildTest {
+
+ /**
+  * As a placeholder, this test should simply return true if the build
+  * process gets far enough to run it. More advanced tests may follow
+  */
+  @Test
+  public void buildTest() {
+	System.out.println("BuildTest ran");
+    assertTrue(true);
+  }
+}
