54d3d2c4939f9071382e05cd198fe8d8041ddd2b
Entreri <marcedholm@gmail.com>
2016-05-26 23:45:08 -0700

    Testing,getters,setters

--- src/main/java/io/github/tryexceptelse/jdex/be/Contact.java
+++ src/main/java/io/github/tryexceptelse/jdex/be/Contact.java
@@ -1,8 +1,76 @@
-package io.github.tryexceptelse.jdex.be;
-
-/**
- * Contact to be stored in Dex
+package io.github.tryexceptelse.jdex;
+/*
+ * one object of this class represents one contact to be added to the list storing contacts.
  */
-public interface Contact {
-
+public class Contact
+{
+    private String first;
+    private String last;
+    private String email;
+    private String streetAddr;
+    private String phoneNumber;
+    private String notes;
+    public void Contact(String newFirst, String newLast, String newEmail, String newStreetAddr, String newPhoneNumber,String newNotes)
+    {
+        first=newFirst;
+        last=newLast;
+        email=newEmail;
+        streetAddr=newStreetAddr;
+        phoneNumber=newPhoneNumber;
+        notes=newNotes;
+    }
+    /*
+     * the following methods can be called to set set the variable of the same name
+     */
+    public void setFirst(String newFirst)
+    {
+        first=newFirst;
+    }
+    public void setLast(String newLast)
+    {
+        last=newLast;
+    }
+    public void setEmai(String newEmail)
+    {
+        email=newEmail;
+    }
+    public void setStreetAddr(String newStreetAddr)
+    {
+        streetAddr=newStreetAddr;
+    }
+    public void setPhoneNumber(String newPhoneNumber)
+    {
+        phoneNumber=newPhoneNumber;
+    }
+    public void setNotes(String newNotes)
+    {
+        notes=newNotes;
+    }
+    /*
+     * the following methods can be called to get variables of the same name
+     */
+    public String getFirst()
+    {
+        return first;
+    }
+    public String getLast()
+    {
+        return last;
+    }
+    public String getEamil()
+    {
+        return email;
+    }
+    public String getstreetAddr()
+    {
+        return streetAddr;
+    }
+    public String getPhoneNumber() 
+    {
+        return phoneNumber;
+    }
+    public String getnotes()
+    {
+        return notes;
+    }
 }
