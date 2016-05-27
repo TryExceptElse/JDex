457f05a393a8c86ebffb60a985b8274afa2c4499
TryExcept <schwarzjohnv@gmail.com>
2016-05-26 21:25:32 -0700

    added fxml, AppWindow skeleton

--- src/main/java/io/github/tryexceptelse/jdex/Settings.java
+++ src/main/java/io/github/tryexceptelse/jdex/Settings.java
@@ -3,8 +3,4 @@ package io.github.tryexceptelse.jdex;
 /**
  * Settings object to store program settings
  */
-class Settings {
-    String autoSaveLoc;
-    String defaultSaveLoc;
-    String lastSavedFile;
-}
+class Settings {} // skeleton
