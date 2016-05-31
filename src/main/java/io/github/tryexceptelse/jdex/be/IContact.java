ae8fa353c91ef9401e6e0f2a328ce5d11d38d013
Entreri <marcedholm@gmail.com>
2016-05-31 11:41:53 -0700

    Refactored Contact class to use HashMap

--- src/main/java/io/github/tryexceptelse/jdex/be/IContact.java
+++ src/main/java/io/github/tryexceptelse/jdex/be/IContact.java
@@ -4,18 +4,8 @@ package io.github.tryexceptelse.jdex.be;
  * Contact Interface. These methods will be called by frontend elements,
  * and so need to be accessible and react predictably
  */
-interface IContact {
-    void setFirst(String newFirst);
-    void setLast(String newLast);
-    void setEmail(String newEmail);
-    void setStreetAddr(String newStreetAddr);
-    void setPhoneNumber(String newPhoneNumber);
-    void setNotes(String newNotes);
-
-    String getFirst();
-    String getLast();
-    String getEmail();
-    String getstreetAddr();
-    String getPhoneNumber();
-    String getnotes();
+interface IContact
+{
+    public void setKey(String key, String value);
+    public String getEntry(String key);
 }
